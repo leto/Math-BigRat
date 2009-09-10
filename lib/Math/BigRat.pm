5110f6249e70e77f35ece86fb24751df56d708ff
Duke Leto <jonathan@leto.net>
2009-09-09 17:43:15 -0700

    Bump version and update CHANGES

--- lib/Math/BigRat.pm
+++ lib/Math/BigRat.pm
@@ -23,7 +23,7 @@ use vars qw($VERSION @ISA $upgrade $downgrade
 
 @ISA = qw(Math::BigFloat);
 
-$VERSION = '0.23';
+$VERSION = '0.24';
 $VERSION = eval $VERSION;
 
 use overload;			# inherit overload from Math::BigFloat
